77b22e032a5b3abda4a4bbef4298fcb45858cc7f
Roshan <roshu190498@gmail.com>
2023-05-10 09:09:40 +0530

    basic seach api added

--- app/src/main/java/com/example/diceassessmentandroid/home/api/HomeApis.kt
+++ app/src/main/java/com/example/diceassessmentandroid/home/api/HomeApis.kt
@@ -1,5 +1,21 @@
 package com.example.diceassessmentandroid.home.api
 
+import com.example.diceassessmentandroid.home.model.SearchApiResponseModel
+import com.example.diceassessmentandroid.network.*
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
 interface HomeApis {
+
+
+    @GET(API_SEARCH)
+    @Headers("$HEADER_SEARCH_API_KEY : $HEADER_SEARCH_API_VALUE")
+    fun getSearchApiReponse(
+        @Query(QAUERY_PARAM_TEXT) queryText : String,
+        @Query(QAUERY_PARAM_SORT) sortBy : String,
+        @Query(QAUERY_PARAM_ORDER) orderBy : String,
+        ) : Call<SearchApiResponseModel>
 
 }
--- app/src/main/java/com/example/diceassessmentandroid/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/diceassessmentandroid/home/viewmodel/HomeViewModel.kt
@@ -1,12 +1,31 @@
 package com.example.diceassessmentandroid.home.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.diceassessmentandroid.common.ResponseData
+import com.example.diceassessmentandroid.common.setError
+import com.example.diceassessmentandroid.common.setLoading
+import com.example.diceassessmentandroid.common.setSuccess
+import com.example.diceassessmentandroid.home.model.SearchApiResponseModel
 import com.example.diceassessmentandroid.home.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel  @Inject constructor(private val homeRepository: HomeRepository) : ViewModel() {
 
-    fun dummytext() = homeRepository.dummyText()
+    val searchApiResponse = MutableLiveData<ResponseData<SearchApiResponseModel>>()
+    fun getTopHeadLine(searchQuery : String, sortBy :String , orderBy : String) {
+        searchApiResponse.setLoading(null)
+        viewModelScope.launch(Dispatchers.IO) {
+            homeRepository.getSearchApiResponse(
+                searchQuery,sortBy,orderBy,
+                {success-> searchApiResponse.setSuccess(success)},
+                {error-> searchApiResponse.setError(error)})
+        }
+    }
+
 }
